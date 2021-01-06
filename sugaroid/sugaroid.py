af1f25f8dcca3f8b47f898c7bd3f5cd0acb94144
Srevin Saju <srevinsaju@sugarlabs.org>
2021-01-06 13:34:15 +0300

    feat: add support for urban dictionary

--- sugaroid/sugaroid.py
+++ sugaroid/sugaroid.py
@@ -350,7 +350,8 @@ class SugaroidBot(ChatBot):
             adapter_type = None
 
         if adapter_type and \
-                adapter_type not in ['NewsAdapter', 'LearnAdapter']:
+                adapter_type not in ['NewsAdapter', 'LearnAdapter',
+                                     'UrbanDictAdapter']:
             if adapter_type in self.globals['history']['types']:
                 if adapter_type == self.globals['history']['types'][-1]:
                     result.text = random_response(REPEAT)
@@ -520,6 +521,7 @@ class Sugaroid:
             'sugaroid.brain.debug.DebugAdapter',
             'sugaroid.brain.interrupt.InterruptAdapter',
             'sugaroid.brain.learn.LearnAdapter',
+            'sugaroid.brain.urbandict.UrbanDictAdapter'
         ]
         self.adapters = [
             'sugaroid.brain.yesno.BoolAdapter',
@@ -689,7 +691,7 @@ class Sugaroid:
                         time=preflight_time,
                         processing_time=delta_time
                     )
-                except PossibleSQLInjectionPanicError:
+                except Exception as e:
                     # to protect our system, we sh
                     pass
             return response
